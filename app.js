5559e16691e6d16bfecbebb56ff30ed4617effc3
Tim-Hinnerk Heuer <heuert@landcareresearch.co.nz>
2015-10-01 09:34:07 +1300

    Renamed some folders, made it work again.

--- app.js
+++ app.js
@@ -81,9 +81,9 @@ var app;
             });
         }]);
 })(app || (app = {}));
-/// <reference path="../Lib/DefinitelyTyped/jquery/jquery.d.ts" />
-/// <reference path="../Lib/DefinitelyTyped/angular/angular.d.ts" />
-/// <reference path="./Services/AppStorage.ts" />
-/// <reference path='./Controllers/AppCtrl.ts' />
-/// <reference path="./Directives/changeName.ts" />
-/// <reference path="./Modules/app.ts" /> 
+/// <reference path="../lib/definitelytyped/jquery/jquery.d.ts" />
+/// <reference path="../lib/definitelytyped/angular/angular.d.ts" />
+/// <reference path="./services/AppStorage.ts" />
+/// <reference path='./controllers/AppCtrl.ts' />
+/// <reference path="./directives/changeName.ts" />
+/// <reference path="./modules/app.ts" />
---
+++ scripts/components/_app.ts
@@ -0,0 +1,6 @@
+﻿/// <reference path="../lib/definitelytyped/jquery/jquery.d.ts" />
+/// <reference path="../lib/definitelytyped/angular/angular.d.ts" />
+/// <reference path="./services/AppStorage.ts" />
+/// <reference path='./controllers/AppCtrl.ts' />
+/// <reference path="./directives/changeName.ts" />
+/// <reference path="./modules/app.ts" />
